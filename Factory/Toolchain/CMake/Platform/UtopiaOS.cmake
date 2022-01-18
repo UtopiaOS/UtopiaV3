741676637dcb741ef4d96e4e4171ee1cab3b5945
Diego Magdaleno <diegomagdaleno@protonmail.com>
2022-01-18 12:57:16 -0600

    [Toolchain] Start working on the CMake toolchain

--- Factory/Toolchain/CMake/Platform/UtopiaOS.cmake
+++ Factory/Toolchain/CMake/Platform/UtopiaOS.cmake
@@ -19,5 +19,5 @@ foreach(type SHARED_LIBRARY SHARED_MODULE EXE)
   set(CMAKE_${type}_LINK_DYNAMIC_C_FLAGS "-Wl,-Bdynamic")
 endforeach()
 
-include(UtopiaPaths)
+include(UtopiaOSPaths)
 include(Platform/UnixPaths)
