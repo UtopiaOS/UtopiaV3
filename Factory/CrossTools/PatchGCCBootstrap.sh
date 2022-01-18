4b3eb0fed3bed0d03d2300083d151c82347643af
Diego Magdaleno <diegomagdaleno@protonmail.com>
2022-01-17 18:37:14 -0600

    [CrossTools] Add new script to patch GCC bootstrap sources

---
+++ Factory/CrossTools/PatchGCCBootstrap.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# Patch mpfr
+pushd $SOURCES_DIR/$MPFR_NAME
+    if [ ! -f ".patched" ]; then
+        patch -Np0 -i "$DIR/CrossTools/Patches/gnu/mpfr/0001-utopia_valid_os.patch"
+        patch -Np0 -i "$DIR/CrossTools/Patches/gnu/mpfr/0002-guess_utopia_libc.patch"
+        touch .patched
+    fi
+popd
+
+# patch gmp
+pushd $SOURCES_DIR/$GMP_NAME
+    if [ ! -f ".patched" ]; then
+        patch -Np0 -i "$DIR/CrossTools/Patches/gnu/gmp/0001-guess_utopia_libc.patch"
+        patch -Np0 -i "$DIR/CrossTools/Patches/gnu/gmp/0002-utopia_valid_os.patch"
+        touch .patched
+    fi
+popd
+
+# patch mpc
+pushd $SOURCES_DIR/$MPC_NAME
+    if [ ! -f ".patched" ]; then
+        patch -Np0 -i "$DIR/CrossTools/Patches/gnu/mpc/0001-utopia_valid_os.patch"
+        patch -Np0 -i "$DIR/CrossTools/Patches/gnu/mpc/0002-detect_utopia_libc.patch"
+        touch .patched
+    fi
+popd
