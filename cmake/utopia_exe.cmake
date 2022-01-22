674d4c0c22021c0541f19d7f6a34119ad66c64a8
Diego Magdaleno <diegomagdaleno@protonmail.com>
2022-01-22 13:36:36 -0600

    [Repo] Remove legacy cmake directory

--- cmake/utopia_exe.cmake
+++
@@ -1,7 +0,0 @@
-FUNCTION(add_utopia_binary bin)
-    foreach(file IN LISTS ARGN)
-        set(files ${files} ${file})
-    endforeach(file)
-
-    add_executable(${bin} ${files})
-ENDFUNCTION(add_utopia_binary)
