2240fdb52963e4c6cd3948b639871b4dfff33d31
Diego Magdaleno <diegomagdaleno@protonmail.com>
2022-01-21 19:19:34 -0600

    [Toolchain] Change the default path when readjusting our toolchain

--- Factory/Toolchain/AdjustToolchain.sh
+++ Factory/Toolchain/AdjustToolchain.sh
@@ -2,7 +2,7 @@ export SPECFILE=`dirname $(${TARGET_TRIPLE}-gcc -print-libgcc-file-name)`/specs
 ${TARGET_TRIPLE}-gcc -dumpspecs > specs
 
 case $(uname -m) in
-    x86_64) sed -i "s|\/lib\/ld-musl-x86_64.so.1|${TOOLS_LOCATION}\/lib\/ld-musl-x86_64.so.1|g" specs
+    x86_64) sed -i "s|\/Core/Binaries\/linkerx86_64|${TOOLS_LOCATION}\/Core\/Binaries\/linkerx86_64|g" specs
             ;;
 esac
 
@@ -16,7 +16,7 @@ echo 'int main(){}' > dummy.c
 ${TARGET_TRIPLE}-gcc dummy.c
 interpreter_path=$(${TARGET_TRIPLE}-readelf -l a.out | grep Requesting | awk '{split($0,a," "); print a[4]}' | sed 's/]//')
 
-expected=$TOOLS_LOCATION/lib/ld-musl-${CONFIG_ARCHITECTURE}.so.1
+expected=$TOOLS_LOCATION/Core/Binaries/linker${CONFIG_ARCHITECTURE}
 
 if [ $interpreter_path != $expected ]; then
     echo "Could not verify that the interpreter path got path critical"
