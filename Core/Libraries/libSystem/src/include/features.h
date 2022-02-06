0b4591826e075feaa7fa19049f6d54229470a6d8
Diego Magdaleno <diegomagdaleno@protonmail.com>
2022-02-05 22:22:59 -0600

    [libSystem] Workaround Mach-O weak aliases

--- Core/Libraries/libSystem/src/include/features.h
+++ Core/Libraries/libSystem/src/include/features.h
@@ -32,7 +32,38 @@
 
 #define weak __attribute__((__weak__))
 #define hidden __attribute__((__visibility__("hidden")))
+
+/*
+ * The following code is... ehhh.. untested and weird to say at least
+ * Utopia uses the Mach-O format as its default binary format (does libcovenant also targets Mach-O)
+ * despite how great the format might be for a lot of things, weak aliasing isn't one of his 
+ * strong sides, weak aliasing on Mach-O is almost non existant? Or not supported? Or not used?
+ * documentation is extremely unclear, nobody documents Mach-O, probably there is some internal 
+ * Apple document which documents all the features of the format?, it looks like there is a 
+ * space in Mach-O files where you would put aliases? in the symbol table of each Mach-O file there
+ * is a N_INDR which is exactly what we want!, but isn't materialized? isn't implemented?
+ * in the Linker? the blog we got this information from is from 2011: http://blog.omega-prime.co.uk/2011/07/06/the-sad-state-of-symbol-aliases/
+ * and no info was made ever since, there is an stackoverflow question! with 8 upvotes
+ * surely someone replied, well no https://stackoverflow.com/questions/49308950/indirect-symbols-in-mach-o-files
+ * there is no reply, the information is weak.
+ * 
+ * So does it have weak aliases? God knows, reexporting symbols is a feature but it is unclear how this
+ * would help?
+ * 
+ * But there is a workaround? sorta? During 2017 our friend Michael Clark started...
+ * well lets say discussing this idea on the llvm mailing lists, and about how lld 
+ * (which can't even statically linkMach-Os...) should support this feature. 
+ * After a long discussion with apple employees that ended up in nothing
+ * he posted some code for weak aliases? do they work? who knows!, i guess time will tell.
+ */
+#if defined (__UTOPIA__)
+#define weak_alias(old, new) \
+        __asm__(".globl _", #new); \
+        __asm__("_" #new " =  _" #old); \
+        extern __typeof (old) new __attribute__((weak_import))
+#else
 #define weak_alias(old, new) \
     extern __typeof(old) new __attribute__((__weak__, __alias__(#old)))
+#endif
 
 #endif /* FEATURES_H */
