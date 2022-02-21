3e9018ef6f05bae181723b72a0badc510eedb35b
Diego Magdaleno <diegomagdaleno@protonmail.com>
2022-02-20 18:58:47 -0600

    [libC] Implement arr_bytes

--- Core/Libraries/libC/include/covenant/std.h
+++ Core/Libraries/libC/include/covenant/std.h
@@ -56,6 +56,7 @@ void c_std_setalloc(ctype_allocfn);
 void *c_arr_data(ctype_arr *);
 usize c_arr_len(ctype_arr *, usize);
 usize c_arr_avail(ctype_arr *);
+usize c_arr_bytes(ctype_arr *);
 
 /* Dynamic */
 ctype_status c_dyn_cat(ctype_arr*, void*, usize, usize);
