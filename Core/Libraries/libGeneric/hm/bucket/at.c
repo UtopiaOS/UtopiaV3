268b34c76489c8705d9b59add055cfff38f6238f
Diego Magdaleno <diegomagdaleno@protonmail.com>
2022-02-26 13:38:19 -0600

    [libGeneric] Implement hm_bucket_at

---
+++ Core/Libraries/libGeneric/hm/bucket/at.c
@@ -0,0 +1,7 @@
+#include <hm.h>
+
+ctype_hm_bucket *
+c_hm_bucket_at(ctype_hmap *hm_map, size idx)
+{
+    return (ctype_hm_bucket*)(((char*)hm_map->buckets)+(hm_map->bucket_size*idx));
+}
--- Core/Libraries/libGeneric/include/covenant/hm.h
+++ Core/Libraries/libGeneric/include/covenant/hm.h
@@ -2,3 +2,6 @@
 #include <covenant/gdat.h>
 
 ctype_hm_bucket c_hm_bucket_init(u64, u64);
+ctype_hm_bucket* c_hm_bucket_at(ctype_hmap*, size);
+ctype_hmap *c_hm_init(size, size, ctype_hashfn, ctype_cmpfn, void(*obj_free)(void *item), void *);
+ctype_status c_hm_insert(ctype_hmap*, void *);
