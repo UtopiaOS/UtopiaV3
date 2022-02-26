57811a0d0bdcdb27656fb32d4a4b0203ea2117de
Diego Magdaleno <diegomagdaleno@protonmail.com>
2022-02-26 17:28:44 -0600

    [libGeneric] Implement c_hm_get

---
+++ Core/Libraries/libGeneric/hm/get.c
@@ -0,0 +1,31 @@
+
+#include <covenant/hm.h>
+#include <covenant/std.h>
+#include <covenant/errcode.h>
+#include <covenant/shared.h>
+#include <covenant/dat.h>
+#include "hm_impl.h"
+
+void *
+c_hm_get(ctype_hmap *hm_map,  const void *key)
+{
+    u64 hash;
+    size i;
+    if (!key) {
+        errno = C_NILOBJ;
+        return nil;
+    }
+    hash = hm_get_hash(hm_map, key);
+    i = hash & hm_map->mask;
+    for (;;) {
+        ctype_hm_bucket* bucket;
+        c_hm_bucket_at(hm_map, i);
+        if (!bucket->dib) {
+            return nil;
+        }
+        if (bucket->hash == hash && hm_map->cmp(key, c_hm_bucket_item(bucket)) == 0) {
+            return c_hm_bucket_item(bucket);
+            i = (i + 1) & hm_map->mask;
+        }
+    }
+}
