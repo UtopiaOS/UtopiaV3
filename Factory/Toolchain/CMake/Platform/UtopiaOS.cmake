c168ec76a17cbf7e059a669c70a224c81f5c02c7
Diego Magdaleno <diegomagdaleno@protonmail.com>
2022-03-13 11:34:33 -0600

    [libKernel] Export dyld_stub_binder and c_kernel

--- Factory/Toolchain/CMake/Platform/UtopiaOS.cmake
+++ Factory/Toolchain/CMake/Platform/UtopiaOS.cmake
@@ -13,7 +13,7 @@ set(CMAKE_SHARED_LIBRARY_RUNTIME_C_FLAG "-Wl,-rpath,")
 #set(CMAKE_SHARED_LIBRARY_RUNTIME_C_FLAG_SEP ":")
 set(CMAKE_SHARED_LIBRARY_RPATH_ORIGIN_TOKEN "\$ORIGIN")
 #set(CMAKE_SHARED_LIBRARY_RPATH_LINK_C_FLAG "-Wl,-rpath-link,")
-set(CMAKE_SHARED_LIBRARY_SONAME_C_FLAG "-install_name")
+#set(CMAKE_SHARED_LIBRARY_SONAME_C_FLAG "-install_name")
 #set(CMAKE_EXE_EXPORTS_C_FLAG "-Wl,--export-dynamic")
 
 # Shared libraries with no builtin soname may not be linked safely by
