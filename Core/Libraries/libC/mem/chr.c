3d4f4ea1aed5e8762973dc4c0bc632b2fa67d301
Diego Magdaleno <diegomagdaleno@protonmail.com>
2022-03-18 20:15:39 -0600

    [libC] Don't define special behaviour on GNU C

--- Core/Libraries/libC/mem/chr.c
+++ Core/Libraries/libC/mem/chr.c
@@ -8,20 +8,15 @@
 #define HASZERO(x) ((x)-ONES & ~(x) & HIGHS)
 
 void *
-c_mem_chr(const void *src, size n, i32 c)
+c_mem_chr(void *src, size n, i32 c)
 {
-    const uchar *s = src;
-    c = (uchar)c;
-    #ifdef __GNUC__
-        for(; ((uintptr)s & ALIGN) && n && *s != c; s++, n--);
-        if (n && *s != c) {
-            typedef size __attribute__((__may_alias__)) word;
-            const word *w;
-            size k = ONES * c;
-            for (w = (const void *)s; n >= SS && !HASZERO(*w^k); w++, n-=SS);
-            s = (const void *)w;
-        }
-    #endif /* __GNUC__ */
-    for (; n && *s != c; s++, n--);
-    return n ? (void *)s : 0;
+    uchar *s;
+
+    s = src;
+    for (; n; --n) {
+        if (*s == c)
+            return s;
+        ++s;
+    }
+    return nil;
 }
