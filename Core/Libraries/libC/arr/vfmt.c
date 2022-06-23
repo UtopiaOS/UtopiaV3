32f2f2300bd3d4627d5f959686ccd85fac825273
Diego Magdaleno <diegomagdaleno@protonmail.com>
2022-06-23 14:49:38 -0500

    [libC] Implement arr vfmt

---
+++ Core/Libraries/libC/arr/vfmt.c
@@ -0,0 +1,15 @@
+#include <covenant/std.h>
+
+static ctype_status
+put(ctype_fmt *p, char *s, usize n) {
+    return c_arr_cat(p->farg, s, n, sizeof(uchar));
+}
+
+size
+c_arr_vfmt(ctype_arr *p, char *fmt, va_list args) {
+    ctype_fmt f;
+    c_fmt_init(&f, p, put);
+
+    va_copy(f.args, args);
+    return c_fmt_fmt(&f, fmt);
+}
