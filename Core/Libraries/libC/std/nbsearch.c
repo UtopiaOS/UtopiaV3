1075b3088287a810d91aee285158b9fc72ebeea3
Diego Magdaleno <diegomagdaleno@protonmail.com>
2022-06-23 15:39:45 -0500

    [libC] Improve nbsearch

--- Core/Libraries/libC/std/nbsearch.c
+++ Core/Libraries/libC/std/nbsearch.c
@@ -19,5 +19,8 @@ c_std_nbsearch(void *k, void *buf, usize m, usize n, ctype_cmpfn fn)
         }
         m >>= 1;
     }
-    return r > 0 ? p : p - n;
+    if ( r > 0) {
+        return p;
+    }
+    return (p == buf) ? p : p - n;
 }
