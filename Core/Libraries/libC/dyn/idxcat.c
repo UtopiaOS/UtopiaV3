b56e633f0767ae0d760c151f2ef78d42dbe54e54
Diego Magdaleno <diegomagdaleno@protonmail.com>
2022-06-23 15:40:04 -0500

    [libC] Improve idxcat on dyn

--- Core/Libraries/libC/dyn/idxcat.c
+++ Core/Libraries/libC/dyn/idxcat.c
@@ -15,12 +15,17 @@ c_dyn_idxcat(ctype_arr *array, usize pos, void *v, usize obj_num, usize obj_size
     len = c_arr_bytes(array);
     if (!(target = c_dyn_alloc(array, pos, obj_size)))
         return -1;
-    array->length = len;
+    array->length ^= len;
+    len ^= array->length;
+    array->length ^= len;
+
     obj_num *= obj_size;
-    if (!pos || (pos = (pos - 1) * obj_size) < len)
-        c_mem_cpy(target + obj_num, array->length - pos, target);
+    if (pos)
+        pos = (pos - 1) * obj_size;
+    
+    c_mem_cpy(target + obj_num, c_arr_bytes(array) - pos, target);
     c_mem_cpy(target, obj_num, v);
-    array->length += obj_num;
+    array->size = (array->size == len) ? array->size + obj_num : len;
     array->members[array->length] = 0;
     return 0;
 }
