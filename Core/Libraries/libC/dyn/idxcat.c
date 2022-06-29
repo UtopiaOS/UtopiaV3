133102ba526dd72863e7ce16134580ae945a925e
Diego Magdaleno <diegomagdaleno@protonmail.com>
2022-06-29 14:11:54 -0500

    [LibC] Fix idxcat

--- Core/Libraries/libC/dyn/idxcat.c
+++ Core/Libraries/libC/dyn/idxcat.c
@@ -1,31 +1,32 @@
 
 
-
 #include <covenant/std.h>
 
 ctype_status
-c_dyn_idxcat(ctype_arr *array, usize pos, void *v, usize obj_num, usize obj_size)
+c_dyn_idxcat(ctype_arr *p, usize pos, void *v, usize m, usize n)
 {
     usize len;
     uchar *target;
 
-    if (c_dyn_ready(array, obj_num, obj_size) < 0)
+    if (c_dyn_ready(p, m, n) < 0)
         return -1;
-    
-    len = c_arr_bytes(array);
-    if (!(target = c_dyn_alloc(array, pos, obj_size)))
+    len = c_arr_bytes(p);
+    if (!(target = c_dyn_alloc(p, pos, n)))
         return -1;
-    array->length ^= len;
-    len ^= array->length;
-    array->length ^= len;
 
-    obj_num *= obj_size;
     if (pos)
-        pos = (pos - 1) * obj_size;
-    
-    c_mem_cpy(target + obj_num, c_arr_bytes(array) - pos, target);
-    c_mem_cpy(target, obj_num, v);
-    array->size = (array->size == len) ? array->size + obj_num : len;
-    array->members[array->length] = 0;
+        pos = (pos - 1) * n;
+    m *= n;
+    if (len > (pos + n))
+    {
+        c_mem_cpy(target + m, len - pos, target);
+        p->length += m;
+    }
+    else
+    {
+        p->length += m - n;
+    }
+    c_mem_cpy(target, m, v);
+    p->members[p->length] = 0;
     return 0;
 }
