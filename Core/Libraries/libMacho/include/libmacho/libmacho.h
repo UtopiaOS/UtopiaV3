184aac06b186c975ef558525706f7819a97ac83b
Diego Magdaleno <diegomagdaleno@protonmail.com>
2023-01-04 14:20:20 -0600

    [libMacho] Redesign LibMacho

--- Core/Libraries/libMacho/include/libmacho/libmacho.h
+++ Core/Libraries/libMacho/include/libmacho/libmacho.h
@@ -10,22 +10,22 @@
 #include <covenant/bits/everytype.h>
 #include <covenant/bool.h>
 
-UTOPIA_ENUM(Int32, cpu_type, ma){
+UTOPIA_ENUM(Int32, MachoCPUType){
     macho_cpu_type_x86_64 = 0x01000007,
 };
 
-UTOPIA_ENUM(Int32, cpu_subtype, ma){
+UTOPIA_ENUM(Int32, MachoCPUSubType){
     macho_cpu_type_x86_64_all = 3,
 };
 
-UTOPIA_ENUM(UInt32, file_type, ma){
+UTOPIA_ENUM(UInt32, MachoFileType){
     macho_file_type_object = 1,
     macho_file_type_executable = 2,
     macho_file_type_dynamic_library = 6,
     macho_file_type_dynamic_linker = 7,
 };
 
-UTOPIA_OPTIONS(UInt32, header_flags, ma)
+UTOPIA_OPTIONS(UInt32, MachoHeaderFlags)
 {
     macho_header_flag_no_undefined_symbols = 1ULL << 0,
     macho_header_flag_dynamically_linked = 1ULL << 2,
@@ -33,21 +33,21 @@ UTOPIA_OPTIONS(UInt32, header_flags, ma)
     macho_header_flag_pie = 1ULL << 21,
 };
 
-UTOPIA_PACKED_STRUCT(macho_header, ma) {
+UTOPIA_PACKED_STRUCT(MachoHeader) {
     UInt32 magic;
-    matype_cpu_type cpu_type;
-    matype_cpu_subtype cpu_subtype;
-    matype_file_type file_type;
+    MachoCPUType cpu_type;
+    MachoCPUSubType cpu_subtype;
+    MachoFileType file_type;
     UInt32 command_count;
     UInt32 total_command_size;
-    matype_header_flags flags;
+    MachoHeaderFlags flags;
     UInt32 reserved;
     char load_commands[];
 };
 
 #define MACHO_MAGIC_64 (0xfeedfacfU)
 
-UTOPIA_ENUM(UInt32, load_command_type, ma) {
+UTOPIA_ENUM(UInt32, MachoLoadCommandType) {
     macho_load_command_type_symbol_table_info = 0x02,
     macho_load_command_type_unix_thread = 0x05,
     macho_load_command_type_dynamic_symbol_table_info = 0x0b,
@@ -58,14 +58,14 @@ UTOPIA_ENUM(UInt32, load_command_type, ma) {
     macho_load_command_type_entry_point = 0x80000028, // ?
 };
 
-UTOPIA_PACKED_STRUCT(load_command, ma) {
-    matype_load_command_type type;
+UTOPIA_PACKED_STRUCT(MachoLoadCommand) {
+    MachoLoadCommandType type;
     UInt32 size;
 };
 
 
-UTOPIA_PACKED_STRUCT(load_command_segment_64, ma) {
-    matype_load_command base;
+UTOPIA_PACKED_STRUCT(MachoLoadCommandSegment64) {
+    MachoLoadCommand base;
     char segment_name[16];
     Int64 memory_address;
     Int64 memory_size;
@@ -77,7 +77,7 @@ UTOPIA_PACKED_STRUCT(load_command_segment_64, ma) {
     UInt32 flags;
 };
 
-UTOPIA_PACKED_STRUCT(section_64, ma) {
+UTOPIA_PACKED_STRUCT(MachoSection64) {
     char section_name[16];
     char segment_name[16];
     Int64 memory_address;
@@ -92,25 +92,25 @@ UTOPIA_PACKED_STRUCT(section_64, ma) {
     UInt32 reserved3;
 };
 
-UTOPIA_PACKED_STRUCT(load_command_thread, ma) {
-    matype_load_command base;
+UTOPIA_PACKED_STRUCT(MachoLoadCommandThread) {
+    MachoLoadCommand base;
 };
 
-UTOPIA_PACKED_STRUCT(load_command_dynamic_linker, ma) {
-    matype_load_command base;
+UTOPIA_PACKED_STRUCT(MachoLoadCommandDynamicLinker) {
+    MachoLoadCommand base;
     UInt32 name_offset;
 };
 
-UTOPIA_PACKED_STRUCT(load_command_dylib, ma) {
-    matype_load_command base;
+UTOPIA_PACKED_STRUCT(MachoLoadCommandDylib) {
+    MachoLoadCommand base;
     UInt32 name_offset;
     UInt32 timestamp;
     UInt32 current_version;
     UInt32 compat_version;
 };
 
-UTOPIA_PACKED_STRUCT(load_command_symbol_table_info, ma) {
-    matype_load_command base;
+UTOPIA_PACKED_STRUCT(MachoLoadCommandSymbolTableInfo) {
+    MachoLoadCommand base;
 
     UInt32 symbol_table_offset;
     UInt32 symbol_table_entry_count;
@@ -119,8 +119,8 @@ UTOPIA_PACKED_STRUCT(load_command_symbol_table_info, ma) {
     UInt32 string_table_size;
 };
 
-UTOPIA_PACKED_STRUCT(load_command_dynamic_symbol_table_info, ma) {
-	matype_load_command base;
+UTOPIA_PACKED_STRUCT(MachoLoadCommandDynamicSymbolTableInfo) {
+	MachoLoadCommand base;
 
 	UInt32 local_symbols_start_index;
 	UInt32 local_symbol_count;
@@ -150,7 +150,7 @@ UTOPIA_PACKED_STRUCT(load_command_dynamic_symbol_table_info, ma) {
 	UInt32 local_relocation_count;
 };
 
-UTOPIA_ENUM(UInt8, symbol_table_entry_type, ma) {
+UTOPIA_ENUM(UInt8, MachoSymbolEntryTableType) {
 	macho_symbol_table_entry_type_undefined = 0,
 	macho_symbol_table_entry_type_absolute  = 1,
 	macho_symbol_table_entry_type_indirect  = 5,
@@ -158,18 +158,18 @@ UTOPIA_ENUM(UInt8, symbol_table_entry_type, ma) {
 	macho_symbol_table_entry_type_section   = 7,
 };
 
-UTOPIA_ENUM(UInt8, symbol_table_entry_section, ma) {
+UTOPIA_ENUM(UInt8, MachoSymbolTableEntrySection) {
 	macho_symbol_table_entry_section_none = 0,
 	// all other values are valid section indicies
 };
 
-UTOPIA_ENUM(UInt8, symbol_table_entry_library_index, ma) {
+UTOPIA_ENUM(UInt8, MachoSymbolTableEntryLibraryIndex) {
 	macho_symbol_table_entry_library_index_self           = 0,
 	macho_symbol_table_entry_library_index_dynamic_lookup = 0xfe,
 	macho_symbol_table_entry_library_index_executable     = 0xff,
 };
 
-UTOPIA_PACKED_STRUCT(symbol_table_entry, ma) {
+UTOPIA_PACKED_STRUCT(MachoSymbolTableEntry) {
 	UInt32 string_table_name_offset;
 	UInt8 type;
 	UInt8 section;
@@ -177,7 +177,7 @@ UTOPIA_PACKED_STRUCT(symbol_table_entry, ma) {
 	Int64 value;
 };
 
-UTOPIA_ALWAYS_INLINE matype_symbol_table_entry_type macho_symbol_table_entry_get_type(UInt8 type_field) {
+UTOPIA_ALWAYS_INLINE MachoSymbolEntryTableType macho_symbol_table_entry_get_type(UInt8 type_field) {
 	return (type_field >> 1) & 7;
 };
 
@@ -204,15 +204,15 @@ UTOPIA_ALWAYS_INLINE bool macho_symbol_table_entry_library_index_is_special(UInt
 	}
 };
 
-UTOPIA_PACKED_STRUCT(load_command_entry_point, ma) {
-	matype_load_command base;
+UTOPIA_PACKED_STRUCT(MachoLoadCommandEntryPoint) {
+	MachoLoadCommand base;
 
 	Int64 entry_offset;
 	Int64 stack_size;
 };
 
-UTOPIA_PACKED_STRUCT(load_command_compressed_dynamic_linker_info, ma) {
-	matype_load_command base;
+UTOPIA_PACKED_STRUCT(MachoLoadCommandCompressedDynamicLinkerInfo) {
+	MachoLoadCommand base;
 
 	UInt32 rebase_info_offset;
 	UInt32 rebase_info_size;
@@ -230,13 +230,13 @@ UTOPIA_PACKED_STRUCT(load_command_compressed_dynamic_linker_info, ma) {
 	UInt32 export_info_size;
 };
 
-UTOPIA_ENUM(UInt8, relocation_type, ma) {
+UTOPIA_ENUM(UInt8, MachoRelocationType) {
 	macho_relocation_type_pointer = 1,
 	macho_relocation_type_text_absolute_32 = 2,
 	macho_relocation_type_text_pc_relative_32 = 3,
 };
 
-UTOPIA_ENUM(UInt8, rebase_opcode, ma) {
+UTOPIA_ENUM(UInt8, MachoRebaseOPCode) {
 	macho_rebase_opcode_done = 0,
 	macho_rebase_opcode_set_type_immediate = 1,
 	macho_rebase_opcode_set_segment_immediate_and_offset_uleb = 2,
@@ -248,7 +248,7 @@ UTOPIA_ENUM(UInt8, rebase_opcode, ma) {
 	macho_rebase_opcode_perform_rebase_uleb_times_skipping_uleb = 8,
 };
 
-UTOPIA_ENUM(UInt8, bind_opcode, ma) {
+UTOPIA_ENUM(UInt8, MachoBindCode) {
 	macho_bind_opcode_done = 0,
 	macho_bind_opcode_set_dylib_ordinal_immediate = 1,
 	macho_bind_opcode_set_dylib_ordinal_uleb = 2,
@@ -265,18 +265,18 @@ UTOPIA_ENUM(UInt8, bind_opcode, ma) {
 	macho_bind_opcode_threaded = 13,
 };
 
-UTOPIA_ENUM(UInt8, bind_subopcode_threaded, ma) {
+UTOPIA_ENUM(UInt8, MachoBindSubOPCodeThreaded) {
 	macho_bind_subopcode_threaded_set_bind_ordinal_table_size_uleb = 0,
 	macho_bind_subopcode_threaded_apply = 1,
 };
 
-UTOPIA_ENUM(UInt8, export_symbol_kind, ma) {
+UTOPIA_ENUM(UInt8, MachoExportSymbolKind) {
 	macho_export_symbol_kind_regular = 0,
 	macho_export_symbol_kind_thread_local = 1,
 	macho_export_symbol_kind_absolute = 2,
 };
 
-UTOPIA_ENUM(UInt8, export_symbol_flags, ma) {
+UTOPIA_ENUM(UInt8, MachoExportSymbolFlags) {
 	macho_export_symbol_flag_weak = 1 << 0,
 	macho_export_symbol_flag_reexport = 1 << 1,
 	macho_export_symbol_flag_stub_and_resolver = 1 << 2,
@@ -290,10 +290,10 @@ UTOPIA_ALWAYS_INLINE UInt8 macho_relocation_instruction_get_immediate(UInt8 byte
 	return byte & 0x0f;
 };
 
-UTOPIA_ALWAYS_INLINE matype_export_symbol_kind macho_export_flags_get_kind(UIntMax raw_flags) {
+UTOPIA_ALWAYS_INLINE MachoExportSymbolKind macho_export_flags_get_kind(UIntMax raw_flags) {
 	return raw_flags & 3ULL;
 };
 
-UTOPIA_ALWAYS_INLINE matype_export_symbol_flags macho_export_flags_get(UIntMax raw_flags) {
+UTOPIA_ALWAYS_INLINE MachoExportSymbolFlags macho_export_flags_get(UIntMax raw_flags) {
 	return raw_flags >> 2;
 };
