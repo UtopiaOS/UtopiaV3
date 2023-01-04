eadf6661fd71103d0504f3574574ff64f39de3c6
Diego Magdaleno <diegomagdaleno@protonmail.com>
2023-01-04 12:47:28 -0600

    [LibC] arr_init now takes unsigned as argument

--- Core/Libraries/libC/arr/init.c
+++ Core/Libraries/libC/arr/init.c
@@ -1,7 +1,7 @@
 #include <covenant/std.h>
 
 Void
-c_arr_init(Array *array, char *s, Size size)
+c_arr_init(Array *array, char *s, USize size)
 {
     array->size = size ? size - 1 : 0;
     array->capacity = 0;
