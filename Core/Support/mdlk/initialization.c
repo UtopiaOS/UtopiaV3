8862285dc7793669ed3b5e0980f57d9e3e178651
Diego Magdaleno <diegomagdaleno@protonmail.com>
2023-01-11 16:22:17 -0600

    [mdlk] Migrate initialization routines to the new data types

--- Core/Support/mdlk/initialization.c
+++ Core/Support/mdlk/initialization.c
@@ -1,22 +1,22 @@
 #include <mdlk/mdlk.h>
 
-static UIntPtr slide_of_executable(const matype_macho_header* program_header) {
+static UIntPtr slide_of_executable(const MachoHeader* program_header) {
     const UInt32 command_count = program_header->command_count;
-    const matype_load_command* const commands = (matype_load_command*)(((char*)program_header) + sizeof(matype_macho_header));
-    const matype_load_command* current_command = commands;
+    const MachoLoadCommand* const commands = (MachoLoadCommand*)(((char*)program_header) + sizeof(MachoHeader));
+    const MachoLoadCommand* current_command = commands;
     for (UInt32 i = 0; i < command_count;  i++) {
         if (current_command->type == macho_load_command_type_segment_64) {
-            const matype_load_command_segment_64* segment_command = (matype_load_command_segment_64*)current_command;
+            const MachoLoadCommandSegment64* segment_command = (MachoLoadCommandSegment64*)current_command;
             if ((segment_command->file_offset == 0) && (segment_command->file_size != 0)) {
                 return (UIntPtr)program_header - segment_command->memory_address;
             }
         }
-        current_command = (const struct matype_load_command*)(((char*)current_command)+current_command->size);
+        current_command = (const struct MachoLoadCommand*)(((char*)current_command)+current_command->size);
 	}
     return 0;
 }  
 
-UIntPtr mdlk_bootstrap_self(const matype_macho_header *program_header, Int32 argc, const char* argv[], IntPtr slide, const matype_macho_header *self_header) 
+UIntPtr mdlk_bootstrap_self(const MachoHeader *program_header, Int32 argc, const char* argv[], IntPtr slide, const MachoHeader *self_header) 
 {
     // In the future we are in fact going to get slided by the kernel, as of now, this wont ever be the case so...
     // TODO: Handle our "self rebase" when we get slided by the kernel
