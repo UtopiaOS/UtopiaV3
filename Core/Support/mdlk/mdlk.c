f07cda31e94f10eef61702e76383b44993cc7db1
Diego Magdaleno <diegomagdaleno@protonmail.com>
2023-01-16 14:19:09 -0600

    [mdlk] Fix wrong mapping on non-pagezero files
    This commit is huge and probably one of the most important in the goal
    of finally loading a Mach-O binary.
    
    It fixes the issue where we would point to the incorrect information
    in the bind and rebase instructions.
    
    This commit fixes said issue by handling non-pagezero files differently
    than files that do have pagezero.
    
    Is recommend to read the comment to get more information

--- Core/Support/mdlk/mdlk.c
+++ Core/Support/mdlk/mdlk.c
@@ -33,6 +33,18 @@ image_entry_new() {
     return some_entry;
 }
 
+static RelocationInfo*
+relocation_info_new() {
+    RelocationInfo* some_relocation_info = c_std_malloc(sizeof(RelocationInfo));
+    some_relocation_info->bind_instructions = nil;
+    some_relocation_info->bind_instructions_size = 0;
+    some_relocation_info->rebase_instrucions = nil;
+    some_relocation_info->rebase_instructions_size = 0;
+    some_relocation_info->weak_bind_instructions = nil;
+    some_relocation_info->weak_bind_instructions_size = 0;
+    return some_relocation_info;
+}
+
 static Int32
 image_entry_dependency_cmp(UniversalType a, UniversalType b) {
     ImageEntryDependency* ia = (ImageEntryDependency*)a;
@@ -91,11 +103,13 @@ ImageEntry *map_macho_object(MachoHeader* program_header, FileDescriptor fd) {
     Macho64Off data_offset;
     Macho64Addr data_virtual_address;
     Macho64Addr data_virtual_limit;
-    UniversalType data_address;
+    UniversalType data_address = nil;
+
+    Status status = StatusOk;
 
     Int32 text_segment = -1;
 
-    RelocationInfo relocation_info = {0};
+    RelocationInfo* relocation_info = relocation_info_new();
 
     Macho64Addr main_address = 0; // Save it before we store it in the actual data structure with our reloc offset
 
@@ -112,6 +126,32 @@ ImageEntry *map_macho_object(MachoHeader* program_header, FileDescriptor fd) {
 
     MachoLoadCommandSegment64* segments[16]; // Temporary pre-slide array of segments...
     UInt32 segment_count = -1;
+
+    UInt32 rebase_info_offset = 0;
+    UInt32 bind_info_offset = 0;
+    UInt32 weak_bind_info_offset = 0;
+
+    Bool with_page_zero = false;
+
+    if (path && current_image->path)
+        c_str_cpy(current_image->path, C_USIZE_MAX, path);
+
+    Int32 linkedit_seg = -1;
+
+    UniversalType some_img_entry = c_hm_get(&images, &(ImageEntry){.path = current_image->path});
+    if (some_img_entry) {
+        c_ioq_fmt(ioq1, "Image already inserted, won't be reloaded\n");
+        status = StatusOk;
+        goto out;
+    }
+
+    if (program_header->total_command_size + sizeof(MachoHeader) > 4*C_PAGESIZE) {
+        c_ioq_fmt(ioq1, "Error: Header too big!\n");
+        status = StatusErr;
+        goto out;
+    }
+
+    c_hm_insert(&images, &(ImageEntry){.path = current_image->path});
 
     // Find:
     // 1. How big is the area we plan on mapping
@@ -131,6 +171,10 @@ ImageEntry *map_macho_object(MachoHeader* program_header, FileDescriptor fd) {
                 if (c_str_equal("__TEXT", C_USIZE_MAX, some_segment_64->segment_name)) {
                     text_segment = (Int32)i;
                 }
+                if (c_str_equal("__PAGEZERO", C_USIZE_MAX, some_segment_64->segment_name)) {
+                    with_page_zero = true;
+                }
+
                 break;
             }
             case macho_load_command_type_entry_point: {
@@ -148,17 +192,18 @@ ImageEntry *map_macho_object(MachoHeader* program_header, FileDescriptor fd) {
             }
             case macho_load_command_type_compressed_dynamic_linker_info_only: {
                 MachoLoadCommandCompressedDynamicLinkerInfo* some_compressed_dynamic_linker_info = (MachoLoadCommandCompressedDynamicLinkerInfo*)some_lc; // Offset by relocbase later in the function
-                relocation_info.rebase_instructions_size = some_compressed_dynamic_linker_info->rebase_info_size;
-                relocation_info.bind_instructions_size = some_compressed_dynamic_linker_info->bind_info_size;
-                relocation_info.weak_bind_instructions_size = some_compressed_dynamic_linker_info->weak_bind_info_size;
+                relocation_info->rebase_instructions_size = some_compressed_dynamic_linker_info->rebase_info_size;
+                relocation_info->bind_instructions_size = some_compressed_dynamic_linker_info->bind_info_size;
+                relocation_info->weak_bind_instructions_size = some_compressed_dynamic_linker_info->weak_bind_info_size;
 
-                relocation_info.rebase_instrucions = (UniversalType)some_compressed_dynamic_linker_info->rebase_info_offset;
-                relocation_info.bind_instructions = (UniversalType)some_compressed_dynamic_linker_info->bind_info_offset;
-                relocation_info.weak_bind_instructions = (UniversalType)some_compressed_dynamic_linker_info->weak_bind_info_offset;
+                rebase_info_offset = some_compressed_dynamic_linker_info->rebase_info_offset;
+                bind_info_offset = some_compressed_dynamic_linker_info->bind_info_offset;
+                weak_bind_info_offset = some_compressed_dynamic_linker_info->weak_bind_info_offset;
 
                 // If you are wondering what a "trie" is, just Google "trie" data structure, I would recommend an article or website, but with current SEO all the websites are trash...
                 current_image->export_trie = (UniversalType)some_compressed_dynamic_linker_info->export_info_offset; // Same story, the address of this is wrong as of now
                 current_image->export_trie_size = some_compressed_dynamic_linker_info->export_info_size;
+                break;
             }
             case macho_load_command_type_load_dynamic_linker: // This is us
             default:
@@ -168,7 +213,10 @@ ImageEntry *map_macho_object(MachoHeader* program_header, FileDescriptor fd) {
     }
     base_virtual_address = trunc_page(segments[0]->memory_address);
     base_virtual_limit = round_page(segments[segment_count]->memory_address + segments[segment_count]->memory_size);
-    map_size = base_virtual_limit - base_virtual_address - segments[0]->memory_size;
+    map_size = base_virtual_limit - base_virtual_address + C_PAGESIZE;
+    // Only substract the first memory section sizes if the segment is PAGEZERO
+    if (c_str_equal(segments[0]->segment_name, C_USIZE_MAX, "__PAGEZERO")) 
+        map_size -= segments[0]->memory_size;
     base_address = (UniversalType)base_virtual_address;
 
     c_ioq_fmt(ioq1, "Virtual address of: %x\n", base_virtual_address);
@@ -194,10 +242,41 @@ ImageEntry *map_macho_object(MachoHeader* program_header, FileDescriptor fd) {
             c_ioq_fmt(ioq1, "Found PAGEZERO segment, won't be overlayed, ignoring\n");
             continue;
         }
-        c_ioq_fmt(ioq1, "Overlaying segment: %s\n", segments[i]->segment_name);
+        // Non obvious error: This wants to calculate our actual data vaddr WITHOUT PAGEZERO, which is assumed to always be
+        // the 0th segment (Unkown if it this is always the case), however, it seems like dylibs don't have PAGEZEROS (duh) and thus
+        // subtracting the first segment memory size results in an overflow/wrong address (I mean this SHOULD be obvious) in order
+        // to prevent this, we will avoid substracting the first segment memory size if we don't have a pagezero.
         data_offset = trunc_page(segments[i]->file_offset);
-        data_virtual_address = trunc_page(segments[i]->memory_address - segments[0]->memory_size);
-        data_virtual_limit = round_page(segments[i]->memory_address - segments[0]->memory_size + segments[i]->file_size);
+        if (with_page_zero) {
+            data_virtual_address = trunc_page(segments[i]->memory_address - segments[0]->memory_size);
+            data_virtual_limit = round_page(segments[i]->memory_address - segments[0]->memory_size + segments[i]->file_size);
+        } else {
+            data_virtual_address = trunc_page(segments[i]->memory_address);
+            /*
+             * Quick note: All of these numbers are hexadicimal
+             * XXX: The weird Mach-O world, when we are on the __DATA segment, these pages are writable, quote from NeXT computers:
+             *
+             * The __DATA segment contains writable data. The static linker sets the virtual memory permissions of this segment to allow both reading and writing. 
+             * Because it is writable, the __DATA segment of a framework or other shared library is logically copied for each process linking with the library.
+             * When memory pages such as those making up the __DATA segment are readable and writable. 
+             * The kernel marks them copy-on-write; therefore when a process writes to one of these pages, that process receives its own private copy of the page.
+             * 
+             * However, these pages also contain data that is unitialized and thus it takes some space on the disk (As in, lets suppose you declare an int), for example, in libC.dylib (as of 16 of jan 2023)
+             * the filesize of the segment is 1000, but the vmsize is 5000 (Unkown if its because of compression). The problem here is with the calculation of limit and sizes (the following varaible).
+             * in this example (libC) __LINKEDIT is next to __DATA, __LINKEDIT, has a "virtual address" of "d000", which is NOT, the result if we were to add, __DATA filesize (1000) with the file offset of the
+             * __LINKEDIT section (9000), however, it is the correct result, if we properly add the "Virtual memory size" instead.
+             * 
+             * Before the change (when we used filesize) we would get segfaulted, as the memory wasn't in consecutive blocks (we need this), after the fix everything went smooth.
+             * 
+             * So, I decided to allocate the full vmsize as the limit, I don't know however, if this is wrong, as I don't have another reference linker to test my theory on
+             * Apple's dyld is unreadable...
+             * 
+             * So it will stay like this, it makes more sense, and the error might have originated from the thought that filesize == memorysize (maybe), but this isn't 
+             * at all the general case.
+             *
+            */
+            data_virtual_limit = round_page(segments[i]->memory_address + segments[i]->memory_size);
+        }
         data_address = map_base + (data_virtual_address - base_virtual_address);
         Int32 protection_flags = macho_flags_to_mmap_protection(segments[i]->initial_memory_protection);
         Int32 data_flags = macho_flags_to_mmap_flags(segments[i]->initial_memory_protection) | C_MAP_FIXED;
@@ -232,15 +311,13 @@ ImageEntry *map_macho_object(MachoHeader* program_header, FileDescriptor fd) {
         current_image->export_trie = (UniversalType)(((PtrDiff)current_image->relocation_base) + ((PtrDiff)current_image->export_trie));
     
     // Fix up relocation info
-    if (relocation_info.bind_instructions)
-        relocation_info.bind_instructions = (UniversalType)(((PtrDiff)current_image->relocation_base) + ((PtrDiff)relocation_info.bind_instructions));
-    if (relocation_info.rebase_instrucions)
-        relocation_info.rebase_instrucions = (UniversalType)(((PtrDiff)current_image->relocation_base) + ((PtrDiff)relocation_info.rebase_instrucions));
-    if (relocation_info.weak_bind_instructions)
-        relocation_info.rebase_instrucions = (UniversalType)(((PtrDiff)current_image->relocation_base) + ((PtrDiff)relocation_info.weak_bind_instructions));
-
-    c_ioq_fmt(ioq1, "Bind instructions address: %p, Rebase instructions address: %p, Weak Bind Address: %p\n", relocation_info.bind_instructions, relocation_info.rebase_instrucions, relocation_info.weak_bind_instructions);
-        
+    if (bind_info_offset != 0)
+        relocation_info->bind_instructions = (UniversalType)(((PtrDiff)current_image->relocation_base) + ((PtrDiff)bind_info_offset));
+    if (rebase_info_offset != 0)
+        relocation_info->rebase_instrucions = (UniversalType)(((PtrDiff)current_image->relocation_base) + ((PtrDiff)rebase_info_offset));
+    if (weak_bind_info_offset != 0)
+        relocation_info->weak_bind_instructions = (UniversalType)(((PtrDiff)current_image->relocation_base) + ((PtrDiff)weak_bind_info_offset));
+    
     current_image->entry = (UniversalType)current_image->relocation_base + main_address;
 
     c_ioq_fmt(ioq1, "Symbol table address: %p, String table address: %p, Export trie address: %p\n", (UniversalType)current_image->symbol_table, (UniversalType)current_image->string_table, (UniversalType)current_image->export_trie);
